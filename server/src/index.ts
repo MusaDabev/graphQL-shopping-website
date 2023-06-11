18c5ee4252379b10875dcbc1a2544e13c9fff586
Musa Dabev <musa_dv@abv.bg>
2023-06-11 10:36:40 +0300

    import typeDefs in index.ts

--- server/src/index.ts
+++ server/src/index.ts
@@ -3,60 +3,9 @@ import { startStandaloneServer } from "@apollo/server/standalone";
 import dotenv from "dotenv";
 import {connectDB} from "./mongo.js";
 import { resolvers } from "./resolvers.js";
+import { typeDefs } from "./typeDefs.js";
 
 dotenv.config();
-
-const typeDefs = `#graphql
-  type Query {
-    getUser(id: ID!): User
-    getUsers: [User!]!
-    getProduct(id: ID!): Product
-    getProducts: [Product]
-  }
-
-  type Mutation {
-    createUser(name: String!, email: String!, password: String!): User,
-    createProduct(
-      title: String!
-      description: String!
-      price: Float!
-      image: String!
-      quantity: Int!
-    ): Product,
-    addToCart(userId: ID!, productId: ID!): User,
-    deleteProduct(id: ID!): Boolean,
-    updateProduct(
-      id: ID!
-      title: String
-      description: String
-      price: Float
-      image: String
-      quantity: Int
-    ): Product,
-    login(email: String!, password: String!): AuthPayload
-    removeFromCart(userId: ID!, itemId: ID!): Boolean
-  }
-
-  type User {
-    id: ID!
-    name: String
-    email: String
-    cart: [Product]
-  }
-
-  type Product {
-    id: ID!
-    title: String
-    description: String
-    price: Float
-    image: String
-    quantity: Int
-  }
-
-  type AuthPayload {
-    user: User!
-  }
-`;
 
 const server = new ApolloServer({
   typeDefs,
